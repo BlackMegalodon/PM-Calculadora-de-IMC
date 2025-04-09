d3ac5bd2a7e3b686744c2b2faaa805be64fe1f47
BlackMegalodon <gabsluck2008@gmail.com>
2025-04-09 11:42:35 -0300

    Update App.js

--- CalculadoraIMC/App.js
+++ CalculadoraIMC/App.js
@@ -1,10 +1,12 @@
 import { StyleSheet, View } from "react-native";
 import Title from "./src/components/Title";
 import FormIMC from "./src/components/FormIMC";
+//importação das informações dos arquivos de componente
 
 export default function App() {
   return (
     <View style={styles.container}>
+    //execução do Title e do FormIMC, o Result.js é iniciado a partir do momento que o IMC é calculado para exibir o resultado por tanto não precisa estar aqui
       <Title />
       <FormIMC />
     </View>
