dbb18c339bfb0a7460f186c185dc4f92000df5d1
BlackMegalodon <gabsluck2008@gmail.com>
2025-04-09 15:49:17 -0300

    Update FormIMC.js

--- CalculadoraIMC/src/components/FormIMC.js
+++ CalculadoraIMC/src/components/FormIMC.js
@@ -3,14 +3,14 @@ import Result from "./Result";
 import { use, useState } from "react";
 
 const FormIMC = () => {
-    //constante criada para preparar e fazer calculo do IMC
+    //Constante criada para preparar e fazer cálculo do IMC.
     const [peso, setPeso] = useState('');
     const [altura, setAltura] = useState('');
     const [imc, setImc] = useState(null);
     const [alturaMetros, setAlturaM] = useState('');
 
     const calcularIMC = () => {
-        //Calculo do IMC e Altura em Metros que serão usados em conjunto para o calculo do peso ideial
+        //Cálculo do IMC e altura em metros que serão usados em conjunto para o cálculo do peso ideal.
         if (peso && altura) {
             const alturaMetros = parseFloat(altura) / 100;
             const imcCalculado = parseFloat(peso) / (alturaMetros * alturaMetros).toFixed(2);
@@ -20,10 +20,10 @@ const FormIMC = () => {
     };
 
     return (
-        //retornado para o usuário a interface
+        //Retornado para o usuário a interface.
         <View style={styles.formContainer}>
             <TextInput
-                //local onde o usuário escrevera seu peso e essa informação será coletada
+                //Local onde o usuário escreverá seu peso e essa informação será coletada.
                 style={styles.input}
                 placeholder="Peso (KG)"
                 keyboardType="numeric"
@@ -31,7 +31,7 @@ const FormIMC = () => {
                 onChangeText={setPeso}
             />
             <TextInput
-                //igual ao anterior o usuário irá inserir sua altura e o sistema coletara
+                //Igual ao anterior, o usuário irá inserir sua altura e o sistema coletará.
                 style={styles.input}
                 placeholder="Altura (CM)"
                 keyboardType="numeric"
@@ -39,8 +39,8 @@ const FormIMC = () => {
                 onChangeText={setAltura}
             />
             <Button title="Calcular IMC" onPress={calcularIMC} />
-                    //botão de confirmação do usuário onde o calculo será feito e depois exibido pelo Result.js
-            {imc && <Result imc={imc} alturaMetros={alturaMetros} />} //transferencia das variaveis para Result.js que serão usadas para exibir informações para o usuário
+                    //Botão de confirmação do usuário, onde o cálculo será feito e depois exibido pelo Result.js.
+            {imc && <Result imc={imc} alturaMetros={alturaMetros} />} //Transferência das variáveis para Result.js que serão usadas para exibir informações para o usuário.
         </View>
     );
 };
@@ -60,5 +60,5 @@ const styles = StyleSheet.create({
         borderRadius: 5,
     },
 });
-//export para confirmar a existencia do FormIMC para o App.js
+//Export para confirmar a existência do FormIMC para o App.js.
 export default FormIMC;
